dc735bd8a7406ed30ddb2cbc5f693c8807226876
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-15 23:25:54 +0200

    nuevos componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { ChoresComponent } from './components/chores/chores.component';
-import { GuardsComponent } from './components/guards/guards.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { Error404Component } from './components/error404/error404.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ChoresComponent,
-    GuardsComponent
+    HeroComponent,
+    NavBarComponent,
+    FooterComponent,
+    LoginComponent,
+    Error404Component,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
