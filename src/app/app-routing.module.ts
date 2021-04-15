702a7ea20efbd6fa22034449a4475272ece79e4d
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-16 01:09:19 +0200

    creando las rutas del home, hero, login, create account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateUserComponent } from './components/user/create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "", pathMatch: 'full', redirectTo: 'hero' },
+  { path: 'home', component: HomeComponent },
+  { path: 'hero', component: HeroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'createaccount', component: CreateUserComponent },
+  { path: '**', component: Error404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
