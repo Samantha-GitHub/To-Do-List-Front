ab619cb1e478f81d1fab5945d47a7d8278c66c14
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-16 10:02:52 +0200

    anadiendola funtionl create user al ts

--- src/app/components/user/create-user/create-user.component.ts
+++ src/app/components/user/create-user/create-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create-user',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateUserComponent implements OnInit {
 
-  constructor() { }
+  formUser: FormGroup;
+
+  constructor(
+    private router: Router, private userService: UserService
+  ) {
+
+    this.formUser = new FormGroup({
+
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,12}$/),
+      ]),
+
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  async onSubmitFormUser(): Promise<any> {
+
+    try {
+
+      const user = await this.userService.create(
+        this.formUser.value
+      );
+      console.log(user);
+
+      // ROUTING TO FORM
+      this.router.navigate(['/login']);
+
+
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
