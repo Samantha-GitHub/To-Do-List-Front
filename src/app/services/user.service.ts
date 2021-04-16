42139a9aac9cedf3ece4eefb4dae33fe03f2a797
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-16 10:02:18 +0200

    anadiendo las funcionalidades al servicio user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,69 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  baseUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = 'http://localhost:3000/api/user';
+  }
+
+  //GET ALL
+
+  getAll(): Promise<User[]> {
+    const httpOptions = { headers: new HttpHeaders() };
+
+    return this.httpClient
+      .get<User[]>(this.baseUrl, httpOptions)
+      .toPromise();
+  }
+
+
+  //GET BY ID TOKEN
+
+  getById(): Promise<User> {
+    return this.httpClient
+      .get<User>(`${this.baseUrl}/profile`, this.createHeaders())
+      .toPromise();
+  }
+
+  //CREATE
+
+  create(formValues): Promise<any> {
+
+    return this.httpClient.post<any>(this.baseUrl, formValues).toPromise();
+  }
+
+  //DELETE
+
+  delete(): Promise<any> {
+    return this.httpClient
+      .put<any>(`${this.baseUrl}`, this.createHeaders())
+      .toPromise();
+  }
+
+
+  //HEADERS
+
+  createHeaders() {
+    return {
+      headers: new HttpHeaders({
+        // 'Content-Type': 'application/json',
+        authorization: localStorage.getItem('to-do-list'),
+      }),
+    };
+  }
+
+  //LOGIN
+
+  login(formValues): Promise<any> {
+    return this.httpClient
+      .post(`${this.baseUrl}/login`, formValues)
+      .toPromise();
+  }
 }
