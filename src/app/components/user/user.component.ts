c4f1d265bc7b6d08cbf161513ed8a41f44eb92a7
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-16 21:37:48 +0200

    se puede actualizar info de user

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  formUser: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private userService: UserService
+
+  ) {
+
+    this.formUser = new FormGroup({
+
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$/),
+      ]),
+      password: new FormControl(),
+
+    })
   }
 
+  async ngOnInit() {
+
+    // Get info user by Id Token
+    this.user = await this.userService.getById();
+    /* console.log('this is user', this.user); */
+
+    // FORM CONTENT
+
+    this.formUser = new FormGroup({
+
+      firstname: new FormControl(this.user.firstname),
+      lastname: new FormControl(this.user.lastname),
+      email: new FormControl(this.user.email),
+      password: new FormControl(this.user.password),
+
+    })
+  }
+
+  async onSubmitFormUser(): Promise<any> {
+
+    try {
+
+      const user = await this.userService.updateById(
+        this.formUser.value
+      );
+      console.log(user);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  refresh(): void {
+    window.location.reload();
+  }
 }
